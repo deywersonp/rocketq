d687afe2dd103a1ed8b28b660616ff3184220293
Deywerson Pereira <deywerson.pereira@gmail.com>
2021-07-29 16:45:13 -0300

    re-write copy-room-data-id

--- public/scripts/copy-room-data-id.js
+++ public/scripts/copy-room-data-id.js
@@ -3,5 +3,7 @@ const copyButton = document.querySelector('#room .buttons #room-id')
 copyButton.addEventListener('click', () => {
   const inputWithRoomId = document.querySelector('#roomIdInput')
   inputWithRoomId.select()
+  inputWithRoomId.setSelectionRange(0, 99999); /* For mobile devices */
   document.execCommand('copy')
+  alert(`Número da sala "${inputWithRoomId.value}" copiado!`);
 })
